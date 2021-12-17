bc526d57b38883b26bfeff05ae77679a7dde0ac9
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2021-12-17 19:56:07 -0300

    initial configs

---
+++ src/components/Modal/modal.tsx
@@ -0,0 +1,48 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure
+} from '@chakra-ui/react'
+
+export default function NewModal() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text fontWeight="bold" mb="1rem">
+              You can scroll the content behind the modal
+            </Text>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+              dolor porro, eius vel ratione eaque quasi maiores assumenda
+              eligendi ea tenetur reprehenderit? Cumque recusandae minima
+              ducimus impedit maiores. Nesciunt, placeat!
+            </p>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
-import Main from 'components/Main'
+import { Box, Input, Stack } from '@chakra-ui/react'
+import Footer from 'components/Footer/footer'
+import NewModal from 'components/Modal/modal'
+import Navbar from 'components/Navbar/navbar'
 
 export default function Home() {
-  return <Main />
+  return (
+    <Box>
+      <Navbar />
+      <Box>
+        <NewModal />
+        <Stack spacing={3}>
+          <Input
+            variant="outline"
+            focusBorderColor="green.500"
+            placeholder="Digite sua meta"
+          />
+        </Stack>
+      </Box>
+      <Footer />
+    </Box>
+  )
 }
