638f00c9c186f2067805a9685b945c005fa0c1da
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2022-01-30 23:38:05 -0300

    feat: add component Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,27 +2,30 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from 'styles/theme'
+import { ListProvider } from 'context/list'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <ChakraProvider resetCSS theme={theme}>
-        <Head>
-          <title>Faz a Meta</title>
+        <ListProvider>
+          <Head>
+            <title>Faz a Meta</title>
 
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+              rel="stylesheet"
+            />
 
-          <meta name="description" content="A simple project" />
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-        </Head>
+            <meta name="description" content="A simple project" />
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+          </Head>
 
-        <Component {...pageProps} />
+          <Component {...pageProps} />
+        </ListProvider>
       </ChakraProvider>
     </>
   )
